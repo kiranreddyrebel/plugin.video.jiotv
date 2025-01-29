a7cb7669e835286143346af4b413723ad7baf913
U.Kiran vas Reddy <35675675+kiranreddyrebel@users.noreply.github.com>
2025-01-29 09:44:25 -0800

    Update constants.py

--- resources/lib/constants.py
+++ resources/lib/constants.py
@@ -32,7 +32,7 @@ EPG_PATH = os.path.join(translatePath(
 
 M3U_CHANNEL = "\n#EXTINF:0 tvg-id=\"{tvg_id}\" tvg-name=\"{channel_name}\" group-title=\"{group_title}\" tvg-chno=\"{tvg_chno}\" tvg-logo=\"{tvg_logo}\"{catchup},{channel_name}\n{play_url}"
 
-EPG_SRC = "https://tobalan.github.io/epg.xml.gz"
+EPG_SRC = "https://raw.githubusercontent.com/testingweb624/jioEpg/refs/heads/main/epg.xml.gz"
 #EPG_SRC = ADDON.getSetting("epgsource")
 
 DICTIONARY_URL = "https://jiotvapi.cdn.jio.com/apis/v1.3/dictionary/dictionary?langId=6"
